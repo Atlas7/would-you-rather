38dbef0e9ed60219c5d7e09510f711b5b655a1ae
atlas7 <johnnychan0302@gmail.com>
2016-08-28 10:44:31 +0100

    basic image embedding with image-webpack-loader

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import { container, title, slogan } from './styles.css'
+import allBlacks from 'file!images/allBlacks.png'
 
 export default function Home () {
   return (
     <div className={container}>
-      <p className={title}>
-        {'Would You Rather'}
-      </p>
-      <p className={slogan}>
-        {'The 100 year old American Classic'}
-      </p>
+      <p className={title}>{'Would You Rather...'}</p>
+      <p className={slogan}>{'this or that'}</p>
+      <img src={allBlacks} width={100} height={100} mode='fit'/>
     </div>
   )
 }
