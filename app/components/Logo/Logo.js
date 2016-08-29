13834ac0b30dc169316cacd34bc4e20b29e9623b
atlas7 <johnnychan0302@gmail.com>
2016-08-29 17:57:31 +0100

    architect a way to resue the Logo component for other images

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,14 +1,17 @@
 import React, { PropTypes} from 'react'
 import allBlacks from 'images/allBlacks.gif'
 
+const defaultImage = allBlacks
+const Logo = ({image=defaultImage, width=50, height=50, mode='fit'}) => {
+  return (
+    <img src={image} width={width} height={height} mode={mode}/>
+  )
+}
+
 Logo.propTypes = {
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
   mode: PropTypes.string.isRequired,
 }
 
-export default function Logo ({width=100, height=100, mode='fit'}) {
-  return (
-    <img src={allBlacks} width={width} height={height} mode={mode}/>
-  )
-}
+export default Logo
